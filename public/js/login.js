ec2fca17b0eb49302f0f199b5bb020b4108a4e7c
wsxxdwd <wsxxdwdd@gamil.com>
2015-01-15 16:49:10 +0800

    登录框阴影

--- public/js/login.js
+++ public/js/login.js
@@ -1,2 +1,9 @@
 $(document).ready(function(){
+    $("#login").click(function(){
+        var username = $("#email").val().trim();
+        var password = $("#password").val();
+        if(username.length == 0 || password.length == 0){
+            alert("用户名密码都不能为空");
+        }
+    });
 });
