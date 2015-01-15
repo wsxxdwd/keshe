d2953c72cae28e723988e69304a5f2106eb14513
hustjzy <752386946@qq.com>
2015-01-15 21:18:32 +0800

    profile.html&user_info.html

--- mms/application/views/profile.php
+++ mms/application/views/profile.php
@@ -5,6 +5,8 @@
 <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />
 <base href = "<?php echo base_url();?>"/> 
 <link href="./public/css/profile.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="./public/css/font-awesome.min.css">
+<link rel="stylesheet" href="./public/css/font-awesome.css">
 <script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.0.js">
 </script>
 <script src="./public/js/profile.js" ></script>
@@ -16,10 +18,30 @@
 	<div id="container">
 		<div class="header">
 			<div class="search">
-				<div class="icon"></div>
+				<div class="content">
+					<i class="fa fa-search">
+						::before
+					</i>
+					<input type="text" placeholder="Search">
+				</div>
+			</div>
+			<div class="info">
+				<div class="head"></div>
+				<div class="username">
+					<div class="wel">welcome</div>
+					<div class="name">wsxxdwd</div>
+				</div>
 			</div>
 		</div>
-		<div id="nav"></div>
+		<div id="nav">
+			<div class="sidebar">
+				<div class="nav_cell dashboard">Dashboard</div>
+				<div class="nav_cell messages">Messages</div>
+				<div class="nav_cell profile">Profile</div>
+				<div class="nav_cell friends">Friends</div>
+				<div class="nav_cell settings">Settings</div>
+			</div>
+		</div>
 	</div>
 </div>
 </body>
